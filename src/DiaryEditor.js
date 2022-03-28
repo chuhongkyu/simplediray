67c4a3d59ba5747db69331e27439683b1d531299
Mr.chu <chuhongkyu@gmail.com>
2022-03-28 09:54:48 +0900

    update

---
+++ src/DiaryEditor.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+
+const DiaryEditor = () =>{
+
+    const [author, setAuthor] = useState("");
+    const [content, setContent] = useState("");
+
+    return (
+        <div className="DiaryEditor">
+        <h2>오늘의 일기</h2>
+        <div>
+            <input value={author}
+            onChange={(e)=>{
+                console.log(e.target.value);
+                setAuthor(e.target.value);
+            }}
+            />
+        </div>
+        <div>
+            <textarea value={content} onChange={(e)=>{
+                setContent(e.target.value);
+            }}
+            />
+        </div>
+    </div>
+    );
+    
+};
+
+export default DiaryEditor;
